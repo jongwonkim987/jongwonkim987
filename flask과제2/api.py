7e617780d7aabd03dbfc3e5950f5ff9f6d64c686
jongwonkim987 <jongwonkim987@gmail.com>
2026-02-05 18:46:07 +0900

    Add files via upload

---
+++ flask과제2/api.py
@@ -0,0 +1,40 @@
+from flask_smorest import Blueprint, abort
+from flask.views import MethodView
+from schemas import BookSchema
+
+blp = Blueprint("books", "books", url_prefix="/books", description="Operations on books")
+
+books = []
+
+@blp.route("/")
+class BookList(MethodView):
+    @blp.response(200, BookSchema(many=True))
+    def get(self):
+        return books
+    @blp.arguments(BookSchema)
+    @blp.response(201, BookSchema)
+    def post(self, new_data):
+        global current_id
+        new_data["id"] = current_id
+        books.append(new_data)
+        current_id += 1
+        return new_data
+
+@blp.route("/<int:book_id>")
+class Book(MethodView):
+    @blp.arguments(BookSchema)
+    @blp.response(200, BookSchema)
+    def put(self, update_data, book_id):
+        book = next((b for b in books if b["id"] == book_id), None)
+        if book is None:
+            abort(404, message="Book not found")
+        book.update(update_data)
+        return book
+
+    def delete(self, book_id):
+        global books
+        book = next((b for b in books if b["id"] == book_id), None)
+        if book is None:
+            abort(404, message="Book not found")
+        books = [b for b in books if b["id"] != book_id]
+        return {"message": "Book deleted"}, 200

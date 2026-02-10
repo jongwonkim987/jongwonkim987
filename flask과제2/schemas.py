3e4ee2be4027606092b9081b0b59f9291e1bfcfb
jongwonkim987 <jongwonkim987@gmail.com>
2026-02-10 18:34:54 +0900

    Delete flask과제2 directory

--- flask과제2/schemas.py
+++
@@ -1,6 +0,0 @@
-from marshmallow import Schema, fields
-
-class BookSchema(Schema):
-    id = fields.Int(dump_only=True)
-    title = fields.Str(required=True)
-    author = fields.Str(required=True)

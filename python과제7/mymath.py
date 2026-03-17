bf7a0c3dcd80ab22faf2edd80b6f2580ca37aaf2
jongwonkim987 <jongwonkim987@gmail.com>
2026-03-17 15:22:16 +0900

    Delete python과제7 directory

--- python과제7/mymath.py
+++
@@ -1,9 +0,0 @@
-import math
-
-def triangle_area(base, height):
-    return (base * height) / 2
-def circle_area(radius):
-    return math.pi * radius * radius
-def box_volume(width, height, depth):
-    return width * height * depth
-
